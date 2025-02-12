1488ee2ebd4465f9bec8e5d0409b5f3f15239841
Artem Simonov <artem.ulm@gmail.com>
2025-02-12 09:59:33 +0300

    add Shop Item View

---
+++ Assets/_Developers/Artem/Scripts/IntValueView.cs
@@ -0,0 +1,6 @@
+namespace MythicalBattles
+{
+    public class IntValueView : ValueView<int>
+    {
+    }
+}
--- Assets/_Developers/Artem/Scripts/ShopItem.cs
+++ Assets/_Developers/Artem/Scripts/ShopItem.cs
@@ -11,5 +11,11 @@ namespace MythicalBattles
         [SerializeField] private Sprite _backgroundImage;
         [SerializeField] private Sprite _itemImage;
         [SerializeField, Range(0,10000)] private int _price;
+
+        public GameObject Model => _model;
+        public Sprite ItemImage => _itemImage;
+        public Sprite BackgroundImage => _backgroundImage;
+        public int Price => _price;
+
     }
 }
