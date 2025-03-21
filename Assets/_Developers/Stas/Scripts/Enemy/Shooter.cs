7a52918bae0a351ca3cc89cc30b6ca0f5e5da2b0
Artem Simonov <artem.ulm@gmail.com>
2025-03-21 19:21:51 +0300

    Fix

---
+++ Assets/_Developers/Stas/Scripts/Enemy/Shooter.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using UnityEngine;
+
+namespace MythicalBattles
+{
+    abstract public class Shooter : MonoBehaviour
+    {
+        [SerializeField] protected ObjectPool _projectilePool;
+        [SerializeField] protected ObjectPool _effectPool;
+        [SerializeField] protected Transform _shootPoint;
+        [SerializeField] protected ParticleSystem _prefire;
+        [SerializeField] protected ParticleSystem _afterfire;
+
+        [SerializeField] protected float _shootSpeed = 1f;
+        [SerializeField] private float _rateOfFire = 1f;
+        [SerializeField] private float _shootDelay = 0.3f;
+
+        protected Transform _transform;
+        protected Animator _animator;
+        private Coroutine _shooter;
+        private WaitForSeconds _sleep;
+        private float _restTimer = 0f;
+
+        private void Awake()
+        {
+            if (_prefire != null)
+                _prefire.Stop();
+
+            if (_afterfire != null)
+                _afterfire.Stop();
+
+            _transform = transform;
+            _animator = GetComponent<Animator>();
+            _animator.SetBool(Constants.IsAttack, false);
+            _sleep = new WaitForSeconds(_rateOfFire);
+        }
+
+        private void Update()
+        {
+            if (_animator.GetBool(Constants.IsDead))
+                return;
+
+            if (!_animator.GetBool(Constants.IsMove) && _animator.GetBool(Constants.IsAttack))
+            {
+                _restTimer += Time.deltaTime;
+
+                if (_prefire != null && _prefire.isStopped)
+                    _prefire.Play();
+
+                if (_afterfire != null && _afterfire.isStopped)
+                    _afterfire.Play();
+
+                if (_restTimer >= _shootDelay)
+                    OnShoot();
+            }
+            else if (_shooter != null)
+            {
+                StopCoroutine(_shooter);
+                _shooter = null;
+                _restTimer = 0f;
+            }
+        }
+
+        protected virtual void Shoot() { }
+
+        private IEnumerator AutoShoot()
+        {
+            while (!_animator.GetBool(Constants.IsDead))
+            {
+                Shoot();
+
+                yield return _sleep;
+            }
+        }
+
+        private void OnShoot() => _shooter ??= StartCoroutine(AutoShoot());
+    }
+}
